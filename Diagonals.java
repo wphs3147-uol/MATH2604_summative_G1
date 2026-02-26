8cce3b0741c40e1ebb57e7b0b2498839db43491f
Sama <dlkq8128@leeds.ac.uk>
2026-02-26 15:19:30 +0000

    Implemented Diagonals sum

--- Diagonals.java
+++ Diagonals.java
@@ -1,6 +1,24 @@
-import static java.lang.Math.*;
+public class Diagonals {
 
-class Diagonals
-{
+    public static double[] sum(double[] A, double[] B) {
+        if (A == null || B == null) {
+            return null;
+        }
+
+        if (A.length != B.length) {
+            return null;
+        }
+
+        double[] result = new double[A.length];
+        for (int i = 0; i < A.length; i++) {
+            result[i] = A[i] + B[i];
+        }
+
+        return result;
+    }
 
 }
+
+
+
+
