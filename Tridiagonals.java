2222518aa6a6384f5e9f3619fc61fd163ce70d48
mpwm0444-uol <mi-a@selman.org>
2026-03-03 12:09:37 +0000

    Implement exampleMatrix for tridiagonal storage

--- Tridiagonals.java
+++ Tridiagonals.java
@@ -4,7 +4,19 @@ class Tridiagonals
 {
 static double[][] exampleMatrix(int n) 
     {
-        double m[][] = new double [1][1]; //dummy code: write your own
-        return m; //dummy code: write your own
+        if (n<=0) throw new IllegalArgumentException ("n must be >0");
+
+        double [][] m = new double [3][n];
+
+        for (int i =0; i< n - 1; i++) {
+            m[0][i] =1;
+        }
+        for (int i=0; i<n; i++) {
+            m[1][i] = -pow (i+1, 2);
+        }
+        for (int i=0; i<n-1; i++) {
+            m[2][i] = i+2;
+        }
+        return m; 
     }
 }
