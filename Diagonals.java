3dcdfb8f581c1999ee5b24ecdb0c8b042054ff47
wphs3147-uol <wphs3147@leeds.ac.uk>
2026-03-09 14:15:04 +0000

    implemented inverse method for diagonal matrices

--- Diagonals.java
+++ Diagonals.java
@@ -1,4 +1,20 @@
-
-
-
-
+import static java.lang.Math.*;
+class Diagonals
+{
+    public static double[] inverse(double[] a)
+    {
+        if (a == null)
+        {
+            return null;
+        }
+        int n = a.length;
+        double[] result = new double[n];
+        for (int i = 0; i < n; i = i + 1)
+        {
+            double value = a[i];
+            double inverseValue = 1.0/value;
+            result[i] = inverseValue;
+        }
+        return result;
+    }
+}
